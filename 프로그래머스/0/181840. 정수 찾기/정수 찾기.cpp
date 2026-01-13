7b12e337a98e41aad81533cb17773cc5b26d1fa9
Kim woojin <149216758+utriver@users.noreply.github.com>
2026-01-13 23:58:53 +0900

    [level 0] Title: 정수 찾기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list, int n) {
+    int answer = 0;
+    for(int num:num_list){
+        if (num == n){
+            answer =1;
+        }
+    }
+    return answer;
+}
