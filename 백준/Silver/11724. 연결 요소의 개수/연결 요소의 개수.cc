5739bffc17d00673575829cf90ce4d2985b49536
Kim woojin <149216758+utriver@users.noreply.github.com>
2026-04-05 08:46:53 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 68 ms, Memory: 6532 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -2,33 +2,42 @@
 #include <vector>
 
 using namespace std;
-int N, M;
-int count_ = 0;
-vector<int> adj[1001];
-bool visit[1001];
-void dfs(int a);
-int main() {
-	cin >> N >> M;
-	for (int i = 0; i < M; i++){
-		int k, s;
-		cin >> k >> s;
-		adj[k].push_back(s);
-		adj[s].push_back(k);
-	}
-	for (int i = 1; i <= N; i++) {
-		if (!visit[i]) {
-			count_++;
-			dfs(i);
-		}
-	}
-	cout << count_ << "\n";
-}
 
-void dfs(int a) {
-	visit[a] = true;
-	for (int next : adj[a]) {
-		if (!visit[next]) {
+int n, m; // 노드와 간선 전역변수 설정
+vector<int> node[1001];
+bool visited[1001];
+void dfs(int cur) {
+	visited[cur] = true;
+	for (int next : node[cur]) {
+		if (visited[next] != true) {
 			dfs(next);
 		}
 	}
+
+}
+
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n >> m; //노드와 간선 수 입력 받기
+	//인접리스트 제작
+	for (int i = 0; i < m; i++) {
+		int k, j;
+		cin >> k >> j;
+		node[k].push_back(j);
+		node[j].push_back(k);
+	}
+	int count = 0;
+	for (int i = 1; i < n+1; i++) {
+		if (visited[i] != true) {
+			dfs(i);
+			count++;
+		}
+	}
+
+	cout << count << "\n";
+
 }
